8963fb1cbb06711fc4a35a64d4a3cc8ec97ef250
Amauri1327 <amauri1816@hotmail.com>
2024-10-24 11:22:50 -0300

    Criacao Product

---
+++ src/main/java/padaria/Bv/model/Product/Product.java
@@ -0,0 +1,23 @@
+package padaria.Bv.model.Product;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    
+}
