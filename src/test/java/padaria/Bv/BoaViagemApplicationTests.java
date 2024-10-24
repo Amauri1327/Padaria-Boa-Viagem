8fb5e083a1bd68498ccd60e348baf20349d4ea9a
Amauri1327 <amauri816@hotmail.com>
2024-10-24 00:20:20 -0300

    arquivos iniciais

---
+++ src/test/java/padaria/Bv/BoaViagemApplicationTests.java
@@ -0,0 +1,13 @@
+package padaria.Bv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoaViagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
