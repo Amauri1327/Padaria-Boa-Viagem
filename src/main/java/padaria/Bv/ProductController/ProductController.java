913800065cfe3eb95eb33544f616860bbb33f39d
Amauri1327 <amauri1816@hotmail.com>
2024-10-26 12:42:47 -0300

    criacao ProductController e metodo Get

---
+++ src/main/java/padaria/Bv/ProductController/ProductController.java
@@ -0,0 +1,27 @@
+package padaria.Bv.ProductController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import padaria.Bv.model.Product.ProductDTO;
+import padaria.Bv.service.ProductService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> findAll(){
+        List<ProductDTO> dto = service.findAll();
+        return ResponseEntity.ok().body(dto);
+    }
+
+}
