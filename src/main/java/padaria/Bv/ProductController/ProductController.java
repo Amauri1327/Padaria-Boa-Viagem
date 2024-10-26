a209ae538687fc337df3093035e35f4d75c2414a
Amauri1327 <amauri1816@hotmail.com>
2024-10-26 12:59:02 -0300

    implementacao Get{/id} e seeds inicial

--- src/main/java/padaria/Bv/ProductController/ProductController.java
+++ src/main/java/padaria/Bv/ProductController/ProductController.java
@@ -3,6 +3,7 @@ package padaria.Bv.ProductController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import padaria.Bv.model.Product.ProductDTO;
@@ -24,4 +25,9 @@ public class ProductController {
         return ResponseEntity.ok().body(dto);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> findById(@PathVariable long id) {
+        ProductDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_products (name, description, price) VALUES ('Produto 1', 'Descrição do Produto 1', 10.50)
+INSERT INTO tb_products (name, description, price) VALUES ('Produto 2', 'Descrição do Produto 2', 20.75)
+INSERT INTO tb_products (name, description, price) VALUES ('Produto 3', 'Descrição do Produto 3', 15.30)
