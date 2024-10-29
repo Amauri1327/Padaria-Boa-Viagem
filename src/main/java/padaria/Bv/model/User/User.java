01f28302c81c0b3f73a79ee1bd65294053b27e07
Amauri1327 <amauri1816@hotmail.com>
2024-10-28 21:02:18 -0300

    implementacao metodo getAuthorities em User

--- src/main/java/padaria/Bv/model/User/User.java
+++ src/main/java/padaria/Bv/model/User/User.java
@@ -3,6 +3,7 @@ package padaria.Bv.model.User;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -18,14 +19,23 @@ import java.util.List;
 public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String login;
     private String password;
+    private UserRole role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+       if(this.role == UserRole.ADMIN){
+           return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+       }
+        if (this.role == UserRole.USER) {
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+        else {
+            return null;
+        }
     }
 
     @Override
