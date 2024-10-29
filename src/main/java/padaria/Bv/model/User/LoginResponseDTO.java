41f02210de51cbca0bd12029ebb7cb671f3ba035
Amauri1327 <amauri1816@hotmail.com>
2024-10-28 21:05:58 -0300

    criacao Dto's registerDTO e LoginResponseDTO

---
+++ src/main/java/padaria/Bv/model/User/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package padaria.Bv.model.User;
+
+public record LoginResponseDTO(String token) {
+}
---
+++ src/main/java/padaria/Bv/model/User/RegisterDTO.java
@@ -0,0 +1,4 @@
+package padaria.Bv.model.User;
+
+public record RegisterDTO(String login, String password, UserRole role) {
+}
