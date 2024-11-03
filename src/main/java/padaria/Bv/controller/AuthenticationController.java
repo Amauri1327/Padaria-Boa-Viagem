7ff6ae274d1a2e66c4baa9a79da9ab951c34d35f
Amauri1327 <amauri1816@hotmail.com>
2024-11-03 20:01:04 -0300

    criacao AuthenticationController

---
+++ src/main/java/padaria/Bv/controller/AuthenticationController.java
@@ -0,0 +1,53 @@
+package padaria.Bv.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import padaria.Bv.infra.security.TokenService;
+import padaria.Bv.model.User.AuthenticationDTO;
+import padaria.Bv.model.User.LoginResponseDTO;
+import padaria.Bv.model.User.RegisterDTO;
+import padaria.Bv.model.User.User;
+import padaria.Bv.repositories.UserRepository;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody AuthenticationDTO data){
+        
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.Login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterDTO data){
+
+        if (this.repository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.login(), encryptedPassword, data.role());
+
+        this.repository.save(newUser);
+        return ResponseEntity.ok().build();
+    }
+}
